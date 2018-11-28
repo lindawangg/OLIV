0522f4b0a084c339b1dcdbc0a2d6c4d100097fb3
lwang <linda.wang513@gmail.com>
2018-11-28 16:57:20 -0500

    runtime evals

--- eval/utils/detect_utils.py
+++ eval/utils/detect_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def detect_objects_no_vis(image_np, sess, detection_graph):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np.expanded = np.expand_dims(image_np, axis=0)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
     # Each box represents a part of the image where a particular object was detected
     boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
