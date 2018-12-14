5b0435605911ceae63407a24a9d24bf54b2a7bf2
lindawangg <linda.wang513@gmail.com>
2018-12-13 21:12:48 -0500

    results for 800k

--- eval/utils/detect_utils.py
+++ eval/utils/detect_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from object_detection.utils import visualization_utils as vis_util
 
 def detect_objects_no_vis(image_np, sess, detection_graph):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
@@ -15,3 +16,34 @@ def detect_objects_no_vis(image_np, sess, detection_graph):
     (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections], feed_dict={image_tensor: image_np_expanded})
 
     return (boxes, scores, classes, num_detections)
+
+def detect_objects(image_np, sess, detection_graph, category_index):
+	# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+	image_np_expanded = np.expand_dims(image_np, axis=0)
+	image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+	# Each box represents a part of the image where a particular object was detected.
+	boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+	# Each score represent how level of confidence for each of the objects.
+	# Score is shown on the result image, together with the class label.
+	scores = detection_graph.get_tensor_by_name('detection_scores:0')
+	classes = detection_graph.get_tensor_by_name('detection_classes:0')
+	num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+	# Actual detection.
+	(boxes, scores, classes, num_detections) = sess.run(
+		[boxes, scores, classes, num_detections],
+		feed_dict={image_tensor: image_np_expanded})
+
+	# Visualization of the results of a detection.
+	vis_util.visualize_boxes_and_labels_on_image_array(
+		image_np,
+		np.squeeze(boxes),
+		np.squeeze(classes).astype(np.int32),
+		np.squeeze(scores),
+		category_index,
+		use_normalized_coordinates=True,
+		line_thickness=8,
+        min_score_thresh=0.5)
+	return image_np
